cc8fbfdaa6463a4dc7e5c8f54519b7455ca1b97d
KevPPC <kev@petpetclub.com.hk>
2024-07-03 14:41:34 +0800

    QR testing Commit

--- src/pages/qr-code/index.jsx
+++ src/pages/qr-code/index.jsx
@@ -1,110 +1,65 @@
-import { useEffect, useRef, useState } from "react";
-
-// Styles
-import "./QrStyles.css";
-
-// Qr Scanner
-import QrScanner from "qr-scanner";
-import QrFrame from "../../assets/qr-frame.svg";
+import "./styles.css";
+import { useState } from "react";
+import QrReader from "react-qr-reader";
 
 const QrCodeContainer = () => {
-  // QR States
-  const scanner = useRef<QrScanner>(null);
-  const videoEl = useRef<HTMLVideoElement>(null);
-  const qrBoxEl = useRef<HTMLDivElement>(null);
-  const [qrOn, setQrOn] = useState<boolean>(true);
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
 
-  // Result
-  const [scannedResult, setScannedResult] = useState<string | undefined>("");
-
-  // Success
-  const onScanSuccess = (result) => {
-    // 🖨 Print the "result" to browser console.
-    console.log(result);
-    // ✅ Handle success.
-    // 😎 You can do whatever you want with the scanned result.
-    setScannedResult(result?.data);
-  };
-
-  // Fail
-  const onScanFail = (err) => {
-    // 🖨 Print the "err" to browser console.
-    console.log(err);
-  };
-
-  useEffect(() => {
-    if (videoEl?.current && !scanner.current) {
-      // 👉 Instantiate the QR Scanner
-      scanner.current = new QrScanner(videoEl?.current, onScanSuccess, {
-        onDecodeError: onScanFail,
-        // 📷 This is the camera facing mode. In mobile devices, "environment" means back camera and "user" means front camera.
-        preferredCamera: "environment",
-        // 🖼 This will help us position our "QrFrame.svg" so that user can only scan when qr code is put in between our QrFrame.svg.
-        highlightScanRegion: true,
-        // 🔥 This will produce a yellow (default color) outline around the qr code that we scan, showing a proof that our qr-scanner is scanning that qr code.
-        highlightCodeOutline: true,
-        // 📦 A custom div which will pair with "highlightScanRegion" option above 👆. This gives us full control over our scan region.
-        overlay: qrBoxEl?.current || undefined,
-      });
-
-      // 🚀 Start QR Scanner
-      scanner?.current
-        ?.start()
-        .then(() => setQrOn(true))
-        .catch((err) => {
-          if (err) setQrOn(false);
-        });
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
     }
-
-    // 🧹 Clean up on unmount.
-    // 🚨 This removes the QR Scanner from rendering and using camera when it is closed or removed from the UI.
-    return () => {
-      if (!videoEl?.current) {
-        scanner?.current?.stop();
-      }
-    };
-  }, []);
-
-  // ❌ If "camera" is not allowed in browser permissions, show an alert.
-  useEffect(() => {
-    if (!qrOn)
-      alert(
-        "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
-      );
-  }, [qrOn]);
-
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
   return (
-    <div className="qr-reader">
-      {/* QR */}
-      <video ref={videoEl}></video>
-      <div ref={qrBoxEl} className="qr-box">
-        <img
-          src={QrFrame}
-          alt="Qr Frame"
-          width={256}
-          height={256}
-          className="qr-frame"
-        />
-      </div>
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>
+        Last Scan:
+        {selected}
+      </h2>
 
-      {/* Show Data Result if scan is success */}
-      {scannedResult && (
-        <p
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 99999,
-            color: "white",
-          }}
-        >
-          Scanned Result: {scannedResult}
-        </p>
+      <button
+        onClick={() => {
+          setStartScan(!startScan);
+        }}
+      >
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
+      {startScan && (
+        <>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Back Camera</option>
+            <option value={"user"}>Front Camera</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
+        </>
       )}
+      {loadingScan && <p>Loading</p>}
+      {data !== "" && <p>{data}</p>}
     </div>
   );
 };
 
 export default QrCodeContainer;
+
 
 
