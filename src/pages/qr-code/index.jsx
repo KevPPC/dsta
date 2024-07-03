f8b00db2211a17fd1493856995688830e6449ad2
KevPPC <kev@petpetclub.com.hk>
2024-07-03 14:49:59 +0800

    6th commit

--- src/pages/qr-code/index.jsx
+++ src/pages/qr-code/index.jsx
@@ -1,63 +1,36 @@
-import "./styles.css";
-import { useState } from "react";
-import QrReader from "react-qr-reader";
+import React from "react";
+import QrReader from "react-qr-scanner";
 
-const QrCodeContainer = () => {
-  const [selected, setSelected] = useState("environment");
-  const [startScan, setStartScan] = useState(false);
-  const [loadingScan, setLoadingScan] = useState(false);
-  const [data, setData] = useState("");
-
-  const handleScan = async (scanData) => {
-    setLoadingScan(true);
-    console.log(`loaded data data`, scanData);
-    if (scanData && scanData !== "") {
-      console.log(`loaded >>>`, scanData);
-      setData(scanData);
-      setStartScan(false);
-      setLoadingScan(false);
-      // setPrecScan(scanData);
-    }
+class QrCodeContainer extends React.Component {
+  state = {
+    delay: 100,
+    result: "No result"
   };
-  const handleError = (err) => {
+
+  handleScan = (data) => {
+    this.setState({
+      result: data
+    });
+  };
+
+  handleError = (err) => {
     console.error(err);
   };
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>
-        Last Scan:
-        {selected}
-      </h2>
 
-      <button
-        onClick={() => {
-          setStartScan(!startScan);
-        }}
-      >
-        {startScan ? "Stop Scan" : "Start Scan"}
-      </button>
-      {startScan && (
-        <>
-          <select onChange={(e) => setSelected(e.target.value)}>
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select>
-          <QrReader
-            facingMode={selected}
-            delay={1000}
-            onError={handleError}
-            onScan={handleScan}
-            // chooseDeviceId={()=>selected}
-            style={{ width: "300px" }}
-          />
-        </>
-      )}
-      {loadingScan && <p>Loading</p>}
-      {data !== "" && <p>{data}</p>}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={this.state.delay}
+          //style={previewStyle}
+          onError={this.handleError}
+          onScan={this.handleScan}
+        />
+        <p>{this.state.result}</p>
+      </div>
+    );
+  }
+}
 
 export default QrCodeContainer;
 
