a1ba8b010d413922a2965621f2c26cf821f5b2fd
KevPPC <kev@petpetclub.com.hk>
2024-07-03 15:30:14 +0800

    latest commit for qr code

--- src/pages/qr-code/index.jsx
+++ src/pages/qr-code/index.jsx
@@ -1,24 +1,52 @@
-import { useState } from "react";
-import { useZxing } from "react-zxing";
+
+import { Html5QrcodeScanner} from "html5-qrcode";
+import { useEffect, useState} from "react";
 
 export const QrCodeContainer = () => {
-  const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      setResult(result.getText());
-    },
-  });
+
+const [scanResult, setScanResult] = useState(null);
+
+  useEffect(() => {
+    const scanner  = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250
+      },
+      fps: 5
+    });
+
+    scanner.render(success, error);
+
+    function success(result) {
+      scanner.clear();
+      setScanResult(result);
+    }
+
+    function error(e) {
+      console.warn(e);
+    }
+  }, []);
 
   return (
     <>
-      <video ref={ref} />
-      <p>
-        <span>Last result:</span>
-        <span>{result}</span>
-      </p>
+     <div className="App">
+      <h1>QR Code Scanning in React</h1>
+      {
+        scanResult
+        ?
+        <div>Success: <a href={"http://" + scanResult}>{scanResult}</a></div>
+        :
+        <div id="reader">Hello</div>
+      }
+    </div>
     </>
   );
 };
+
+
+
+
+
 
 
 
