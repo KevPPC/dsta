a3fa584596112c1f69ed991901ef1f575488068e
KevPPC <kev@petpetclub.com.hk>
2024-07-08 15:27:09 +0800

    Changes UI

--- src/pages/qr-code/index.jsx
+++ src/pages/qr-code/index.jsx
@@ -1,37 +1,45 @@
-import { Html5QrcodeScanner} from "html5-qrcode";
+import { Html5QrcodeScanner, Html5Qrcode} from "html5-qrcode";
 import { useEffect, useState} from "react";
 
 export const QrCodeContainer = () => {
 
-    const [scanResult, setScanResult] = useState(null);
+  const [scanResult, setScanResult] = useState(null);
+
 
   useEffect(() => {
     
-    const scanner  = new Html5QrcodeScanner('reader', {
+    const scanner  = new Html5Qrcode('reader');
+
+    const config = {
       qrbox: {
         width: 250,
         height: 250
       },
       fps: 5,
-      videoConstraints: {
-        facingMode: { exact: "environment" },
-    },
-    }, false);
+    }
 
-    scanner.render(success, error);
-
-    function success(result) {
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+      console.log(decodedResult);
       scanner.clear();
-      setScanResult(result);
-    }
+      setScanResult(decodedResult);
+    };
 
-    function error(e) {
-      console.warn(e);
-    }
+    scanner.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+
+    // function success(result) {
+    //   scanner.clear();
+    //   setScanResult(result);
+    // }
+
+    // function error(e) {
+    //   console.warn(e);
+    // }
   }, []);
 
   return (
     <>
+    <div id="reader" width="600px"></div>
      <div className="App">
       <h1>QR Code Scanner</h1>
       {
